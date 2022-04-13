b0d89c0319b51f096a1dc0b0f507b7656cbf1d02
Davinder Pal Singh <d0s0fev@homeoffice.wal-mart.com>
2022-04-13 13:03:01 +0530

    Revert "adding new function sum"
    This reverts commit 238a07edc41ba93c4396b149603dcab79620afc7.

--- data_processing.py
+++ data_processing.py
@@ -2,13 +2,3 @@
 
 
 print("Adding data processing code for fraud detection")
-
-a = 5
-b = 7
-sum(a,b)
-
-def sum(a, b):
-    c = a + b
-    return a
-
-print("Added new function sum() which is not correct")
