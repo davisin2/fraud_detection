0c9c00d96f96eee868ebebca5af50018d8cc356c
Davinder Pal Singh <d0s0fev@homeoffice.wal-mart.com>
2022-04-13 13:02:56 +0530

    Revert "correct code for sum and adding multiplication"
    This reverts commit 1f7f9d470f95318f7c3458874f0a16d9e0b37e54.

--- data_processing.py
+++ data_processing.py
@@ -2,22 +2,3 @@
 
 
 print("Adding data processing code for fraud detection")
-
-
-a = 5
-b = 7
-print("sum func")
-
-print(sum(a,b))
-
-def sum(a, b):
-    c = a + b
-    return c
-
-def multiplication(a,b):
-    d = a*b
-    return b
-
-print("multiplication func")
-print(multiplication(5,7))
-
