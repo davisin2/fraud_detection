dbf2a56f3fa0b6fc78a14b5621a2ebc1aef0a3b2
Davinder Pal Singh <d0s0fev@homeoffice.wal-mart.com>
2022-04-13 13:02:54 +0530

    Revert "added new func for mod"
    This reverts commit 355a879af7a11c2e9d6dab3c90b307ff0204ba00.

--- data_processing.py
+++ data_processing.py
@@ -21,10 +21,3 @@ def multiplication(a,b):
 print("multiplication func")
 print(multiplication(5,7))
 
-
-def mod_func(a, b):
-    e = a/b
-    return a
-
-print("mod_func func")
-print(mod_func(5,7))
